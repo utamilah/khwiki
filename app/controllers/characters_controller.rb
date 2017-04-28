3173e8cc9bd43408a833e63c0e5db9c65efe62fb
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-28 15:29:28 -0400

    get character CRUD functioning

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,3 +1,47 @@
 class CharactersController < ApplicationController
-  
+  def index
+    @characters = Character.all
+  end
+
+  def show
+    @character = Character.find(params[:id])
+  end
+
+  def new
+    @world = World.find(params[:world_id])
+    @character = Character.new
+  end
+
+  def create
+    @world = World.find(params[:world_id])
+    @character = @world.characters.create(character_params)
+
+    redirect_to world_character_path(@world, @character)
+  end
+
+  def edit
+    @world = World.find(params[:world_id])
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @world = World.find(params[:world_id])
+    @character = Character.find(params[:id])
+    @character.update(character_params)
+
+    redirect_to world_character_path(@character)
+  end
+
+  def destroy
+    @world = World.find(params[:world_id])
+    @character = Character.find(params[:id])
+    @character.destroy
+
+    redirect_to world_path(@world)
+  end
+
+  private
+    def character_params
+      params.require(:character).permit(:name, :img_url, :description)
+    end
 end
