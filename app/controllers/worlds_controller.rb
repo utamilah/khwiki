f2384542dbf75dcb848da35a8b06441b9f0d08bd
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-28 15:06:23 -0400

    full CRUD for worlds

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -22,14 +22,19 @@ class WorldsController < ApplicationController
   def update
     @world = World.find(params[:id])
     @world.update(world_params)
+
+    redirect_to world_path
   end
 
   def destroy
+    @world = World.find(params[:id])
+    @world.destroy
 
+    redirect_to worlds_path
   end
 
   private
     def world_params
-      params.require(:world_params).permit(:name, :img_url, :description)
+      params.require(:world).permit(:name, :img_url, :description)
     end
 end
